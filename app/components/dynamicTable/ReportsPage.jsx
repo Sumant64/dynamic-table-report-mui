b478875526f9503649f0335e9a4fe85f93f808e4
sumantgigacars <sumant.shah@gigacars.com>
2024-12-09 15:37:01 +0530

    pagination implementation

--- app/components/dynamicTable/ReportsPage.jsx
+++ app/components/dynamicTable/ReportsPage.jsx
@@ -1,18 +1,40 @@
 'use client'
 import { Box } from '@mui/material';
-import React, { useState } from 'react';
+import React, { useEffect, useState } from 'react';
 import data from '@/data/data.json'
 import { columnData } from '@/utils/tableHeader';
 import ReportsTable from './ReportsTable';
 
 const ReportsPage = () => {
-    const [rows, setRows] = useState([...data]);
+    const [rowsPerPage, setRowsPerPage] = useState(10);
     const [columns, setColumns] = useState([...columnData]);
+    const [rows, setRows] = useState([]);
+    const [page, setPage] = useState(1);
+    const [count, setCount] = useState(0);
+
+    useEffect(() => {
+        initialLoad();
+    }, [page, rowsPerPage])
+
+    const initialLoad = () => {
+        try{
+            let dataArr = [...data];
+            console.log(dataArr)
+
+            let paginatedData = dataArr.slice(page * rowsPerPage - rowsPerPage, page * rowsPerPage);
+            setCount(dataArr.length);
+            setRows(paginatedData);
+            console.log(paginatedData, "==========")
+
+        } catch(err) {
+            console.log(err);
+        }
+    }
 
 
   return (
     <Box paddingTop={"20px"}>
-        <ReportsTable columns={columns} rows={rows} />
+        <ReportsTable setRowsPerPage={setRowsPerPage} page={page} setPage={setPage} columns={columns} count={count} rowsPerPage={rowsPerPage} rows={rows} />
     </Box>
   )
 }
--- app/components/dynamicTable/ReportsTable.jsx
+++ app/components/dynamicTable/ReportsTable.jsx
@@ -1,38 +1,43 @@
-import { Paper, styled, Table, TableBody, TableCell, tableCellClasses, TableContainer, TableHead, TableRow } from '@mui/material';
+import { Paper, styled, Table, TableBody, TableCell, tableCellClasses, TableContainer, TableHead, TablePagination, TableRow } from '@mui/material';
 import React from 'react'
 
-const ReportsTable = ({columns, rows}) => {
+const ReportsTable = ({ columns, page, rowsPerPage, count, rows, setPage, setRowsPerPage }) => {
 
-    const StyledTableCell = styled(TableCell)(({ theme }) => ({
-        [`&.${tableCellClasses.head}`]: {
-            backgroundColor: "#2C387E",
-            color: theme.palette.common.white,
-        },
-        [`&.${tableCellClasses.body}`]: {
-            fontSize: 14,
-        },
-    }));
-    
-    const StyledTableRow = styled(TableRow)(({ theme }) => ({
-        '&:nth-of-type(odd)': {
-            backgroundColor: theme.palette.action.hover,
-        },
-        // hide last border
-        '&:last-child td, &:last-child th': {
-            border: 0,
-        },
-    }));
+  const StyledTableCell = styled(TableCell)(({ theme }) => ({
+    [`&.${tableCellClasses.head}`]: {
+      backgroundColor: "#2C387E",
+      color: theme.palette.common.white,
+    },
+    [`&.${tableCellClasses.body}`]: {
+      fontSize: 14,
+    },
+  }));
+
+  const StyledTableRow = styled(TableRow)(({ theme }) => ({
+    '&:nth-of-type(odd)': {
+      backgroundColor: theme.palette.action.hover,
+    },
+    // hide last border
+    '&:last-child td, &:last-child th': {
+      border: 0,
+    },
+  }));
+
+  const handleRowsPerPageChange = () => {
+    setRowsPerPage(parseInt(event.target.value, 10));
+    setPage(1);
+  }
 
   return (
     <>
-        {/* table section */}
+      {/* table section */}
       <TableContainer sx={{ height: '70vh' }} component={Paper}>
         <Table sx={{ minWidth: 700 }} aria-label="customized table">
           <TableHead sx={{ position: 'sticky', top: '0px', zIndex: '5' }}>
             <TableRow>
               {
                 columns.map((item, index) => {
-                  if(item.display) {
+                  if (item.display) {
                     return (
                       <StyledTableCell key={index} sx={{ minWidth: item?.width }}>
                         {item.field}
@@ -42,28 +47,58 @@ const ReportsTable = ({columns, rows}) => {
                 })
               }
             </TableRow>
-            </TableHead>
-            <TableBody>
-              {
-                rows?.length > 0 && rows.map((item, index) => {
-                  return (
-                    <StyledTableRow key={index}>
-                      {columns[columns.findIndex(item => item.field === 'ID' && item.display)] && <StyledTableCell>{item.id}</StyledTableCell>}
-                      {columns[columns.findIndex(item => item.field === 'Created On' && item.display)] && <StyledTableCell>{item.createdOn}</StyledTableCell>}
-                      {columns[columns.findIndex(item => item.field === 'Payer' && item.display)] && <StyledTableCell>{item.payer}</StyledTableCell>}
-                      {columns[columns.findIndex(item => item.field === 'Status' && item.display)] && <StyledTableCell>{item.status}</StyledTableCell>}
-                      {columns[columns.findIndex(item => item.field === 'Email' && item.display)] && <StyledTableCell>{item.email}</StyledTableCell>}
-                      {columns[columns.findIndex(item => item.field === 'Payer Phone' && item.display)] && <StyledTableCell>{item.payerPhone}</StyledTableCell>}
-                      {columns[columns.findIndex(item => item.field === 'Services' && item.display)] && <StyledTableCell>{item.services}</StyledTableCell>}
-                      {columns[columns.findIndex(item => item.field === 'Scheduled' && item.display)] && <StyledTableCell>{item.scheduled}</StyledTableCell>}
+          </TableHead>
+          <TableBody>
+            {
+              rows?.length > 0 && rows.map((item, index) => {
+                return (
+                  <StyledTableRow key={index}>
+                    {columns[columns.findIndex(item => item.field === 'ID' && item.display)] && <StyledTableCell>{item.id}</StyledTableCell>}
+                    {columns[columns.findIndex(item => item.field === 'Created On' && item.display)] && <StyledTableCell>{item.createdOn}</StyledTableCell>}
+                    {columns[columns.findIndex(item => item.field === 'Payer' && item.display)] && <StyledTableCell>{item.payer}</StyledTableCell>}
+                    {columns[columns.findIndex(item => item.field === 'Status' && item.display)] && <StyledTableCell>{item.status}</StyledTableCell>}
+                    {columns[columns.findIndex(item => item.field === 'Email' && item.display)] && <StyledTableCell>{item.email}</StyledTableCell>}
+                    {columns[columns.findIndex(item => item.field === 'Payer Phone' && item.display)] && <StyledTableCell>{item.payerPhone}</StyledTableCell>}
+                    {columns[columns.findIndex(item => item.field === 'Services' && item.display)] && <StyledTableCell>{item.services}</StyledTableCell>}
+                    {columns[columns.findIndex(item => item.field === 'Scheduled' && item.display)] && <StyledTableCell>{item.scheduled}</StyledTableCell>}
 
-                    </StyledTableRow>
-                  )
-                })
-              }
-            </TableBody>
+                  </StyledTableRow>
+                )
+              })
+            }
+          </TableBody>
         </Table>
       </TableContainer>
+
+      {/* table pagination */}
+      {rows.length > 0 && (
+        <TablePagination
+          component={Paper}
+          elevation={2}
+          sx={{
+            "& .MuiTablePagination-selectLabel": {
+              margin: '0px'
+            },
+            "& .MuiTablePagination-displayedRows": {
+              margin: '0px'
+            },
+            "& .MuiTablePagination-input .MuiSvgIcon-fontSizeMedium": {
+              color: '#fff'
+            },
+            // marginTop: '-10px',
+            // margin: { xs: "5px 0px 0px -5px", md: "5px 0px" },
+            backgroundColor: "#2C387E",
+            color: "#fff",
+            bottom: "0px",
+            width: "100%",
+          }}
+          count={count}
+          page={page - 1}
+          onPageChange={(event, newPage) => setPage(newPage + 1)}
+          rowsPerPage={rowsPerPage}
+          onRowsPerPageChange={handleRowsPerPageChange}
+        />
+      )}
     </>
   )
 }
