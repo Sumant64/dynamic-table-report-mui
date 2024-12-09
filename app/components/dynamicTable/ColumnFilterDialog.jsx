8072f756a1682e75a284d9464c50ebe662b4715e
sumantgigacars <sumant.shah@gigacars.com>
2024-12-09 16:36:44 +0530

    filter dialog

---
+++ app/components/dynamicTable/ColumnFilterDialog.jsx
@@ -0,0 +1,133 @@
+import { Box, Button, Checkbox, FormControlLabel, FormGroup } from '@mui/material';
+import React, { useState } from 'react';
+import { IoIosCloseCircle } from "react-icons/io";
+
+const ColumnFilterDialog = (props) => {
+    const [checkList, setCheckList] = useState(props.columns);
+
+
+    const handleChange = (item) => {
+        let newCheckList = JSON.parse(JSON.stringify(checkList));
+        newCheckList.forEach((value) => {
+            if (value.field === item.field) {
+                value.display = !value.display;
+            }
+        })
+
+        setCheckList(newCheckList);
+    }
+
+    const handleReset = () => {
+        let newCheckList = JSON.parse(JSON.stringify(checkList));
+        newCheckList.forEach((value) => {
+            value.display = true;
+        })
+        setCheckList(newCheckList);
+        props.setColumns(newCheckList);
+        props.setOpenDialog(false)
+    }
+
+    const handleApply = () => {
+        props.setColumns(checkList);
+        props.setOpenDialog(false)
+    }
+
+    return (
+        <>
+            <Box
+                sx={{
+                    paddingRight: '1rem',
+                    paddingLeft: '1rem',
+                    paddingTop: '1rem'
+                }}
+            >
+                {/* header */}
+                <Box sx={{
+                    display: 'flex',
+                    justifyContent: 'space-between',
+                }}>
+                    <h3>Edit Column</h3>
+                    <IoIosCloseCircle style={{
+                        width: '25px',
+                        height: '25px',
+                        position: 'relative',
+                        // top: '12px',
+                        marginLeft: '2rem',
+                        cursor: 'pointer'
+                    }} onClick={() => props.setOpenDialog(false)} />
+                </Box>
+
+                {/* Options */}
+                <Box
+                    sx={{
+                        maxHeight: '250px',
+                        overflowY: 'auto',
+                        /* width */
+                        "::-webkit-scrollbar": {
+                            width: "5px"
+                        },
+
+                        /* Track */
+                        "::-webkit-scrollbar-track": {
+                            background: "#f1f1f1"
+                        },
+
+                        /* Handle */
+                        "::-webkit-scrollbar-thumb": {
+                            background: "#888"
+                        },
+
+                        /* Handle on hover */
+                        "::-webkit-scrollbar-thumb:hover": {
+                            background: "#555"
+                        }
+                    }}
+                >
+                    <FormGroup>
+                        {
+                            checkList?.length > 0 && checkList.map((item, index) => (
+                                <FormControlLabel key={index} control={<Checkbox checked={item.display} onChange={() => handleChange(item)} />} label={item.field} />
+                            ))
+                        }
+                    </FormGroup>
+                </Box>
+
+            </Box>
+            {/* Buttons */}
+            <Box
+                sx={{
+                    display: 'flex',
+                    justifyContent: 'space-between',
+                    padding: '10px'
+                }}
+            >
+                <Button
+                    sx={{
+                        color: "white",
+                        backgroundColor: '#2C387E',
+                        fontSize: '12px',
+                        width: '80px',
+                        "&:hover": {
+                            backgroundColor: "#8088b2",
+                        },
+                    }}
+                    onClick={() => handleReset()}
+                >Reset</Button>
+                <Button
+                    sx={{
+                        color: "white",
+                        backgroundColor: '#2C387E',
+                        fontSize: '12px',
+                        width: '80px',
+                        "&:hover": {
+                            backgroundColor: "#8088b2",
+                        },
+                    }}
+                    onClick={() => handleApply()}
+                >Apply</Button>
+            </Box>
+        </>
+    )
+}
+
+export default ColumnFilterDialog
--- app/components/dynamicTable/ReportsPage.jsx
+++ app/components/dynamicTable/ReportsPage.jsx
@@ -1,9 +1,12 @@
 'use client'
-import { Box } from '@mui/material';
+import { Box, Dialog, Paper } from '@mui/material';
 import React, { useEffect, useState } from 'react';
 import data from '@/data/data.json'
 import { columnData } from '@/utils/tableHeader';
 import ReportsTable from './ReportsTable';
+import { CiFilter } from "react-icons/ci";
+import { LuDownload } from "react-icons/lu";
+import ColumnFilterDialog from './ColumnFilterDialog';
 
 const ReportsPage = () => {
     const [rowsPerPage, setRowsPerPage] = useState(10);
@@ -12,29 +15,52 @@ const ReportsPage = () => {
     const [page, setPage] = useState(1);
     const [count, setCount] = useState(0);
 
+    const [openDialog, setOpenDialog] = useState('');
+
     useEffect(() => {
         initialLoad();
     }, [page, rowsPerPage])
 
     const initialLoad = () => {
-        try{
+        try {
             let dataArr = [...data];
 
             let paginatedData = dataArr.slice(page * rowsPerPage - rowsPerPage, page * rowsPerPage);
             setCount(dataArr.length);
             setRows(paginatedData);
 
-        } catch(err) {
+        } catch (err) {
             console.log(err);
         }
     }
 
 
-  return (
-    <Box paddingTop={"20px"}>
-        <ReportsTable setRowsPerPage={setRowsPerPage} page={page} setPage={setPage} columns={columns} count={count} rowsPerPage={rowsPerPage} rows={rows} />
-    </Box>
-  )
+    return (
+        <>
+            {/* Columns filter dialog */}
+            <Dialog open={openDialog==="filter"} onClose={() => setOpenDialog("")}>
+                <ColumnFilterDialog columns={columns} setColumns={setColumns} setOpenDialog={setOpenDialog} />
+            </Dialog>
+
+
+            <Box paddingTop={"20px"}>
+                {/* Options Container */}
+                <Box sx={{
+                    display: 'inline-flex',
+                    marginBottom: '10px'
+                }} component={Paper} elevation={3}>
+                    <Box sx={{ cursor: 'pointer', padding: '4px 10px', borderRight: '1px solid black' }} onClick={() => setOpenDialog('filter')}>
+                        <CiFilter />
+                    </Box>
+                    <Box sx={{ cursor: 'pointer', padding: '4px 10px' }}>
+                        <LuDownload />
+                    </Box>
+                </Box>
+
+                <ReportsTable setRowsPerPage={setRowsPerPage} page={page} setPage={setPage} columns={columns} count={count} rowsPerPage={rowsPerPage} rows={rows} />
+            </Box>
+        </>
+    )
 }
 
 export default ReportsPage
