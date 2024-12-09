b67d5be43196029a796c455cc07e5b935fd5e91a
sumantgigacars <sumant.shah@gigacars.com>
2024-12-09 12:16:31 +0530

    top bar toggle integration

--- app/components/layout/Topbar.jsx
+++ app/components/layout/Topbar.jsx
@@ -6,23 +6,37 @@ import XIcon from '@mui/icons-material/X';
 
 
 const Topbar = (props) => {
+
+    const handleMobileSidebar = () => {
+        props.setToggle(!props.toggle);
+    }
+
     return (
         <>
             <AppBar elevation={3} sx={{ backgroundColor: "#e0e0e0" }}>
                 <Toolbar sx={{ display: 'flex', justifyContent: 'space-between' }}>
                     {/* Options */}
-                    <Box display="flex" sx={{gap: '10px'}}>
-                        <XIcon sx={{color: '#616161', position: 'relative', top: '3px'}} />
-                        <Typography variant='h5' sx={{color: '#616161'}}>REPORTS</Typography>
+                    <Box display="flex" sx={{ gap: '10px' }}>
+                        <XIcon sx={{ color: '#616161', position: 'relative', top: '3px' }} />
+                        <Typography variant='h5' sx={{ color: '#616161' }}>REPORTS</Typography>
                     </Box>
 
                     {/* Interactions */}
                     <Box display='flex' sx={{ width: '100%', justifyContent: 'end' }}>
                         <IconButton title="log out">
-                            <InputIcon />
+                            <InputIcon sx={{ fontSize: '28px' }} />
                         </IconButton>
-                        <IconButton title="log out">
-                            <DehazeIcon />
+                        <IconButton
+                            sx={{
+                                display: { xs: 'block', md: 'none' }
+                            }}
+                            onClick={handleMobileSidebar}
+                        >
+                            <DehazeIcon sx={{
+                                fontSize: '28px',
+                                position: 'relative',
+                                top: '2px'
+                            }} />
                         </IconButton>
                     </Box>
                 </Toolbar>
